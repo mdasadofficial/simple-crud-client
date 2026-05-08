42115eeb12e7b7f45acc095c29642b25a9e848cf
mdasadofficial <mdasadofficial1@gmail.com>
2026-05-08 22:47:14 +0600

    Add delete confirmation dialog to UsersTable and restore getUserById functionality

--- src/app/components/usersTable.jsx
+++ src/app/components/usersTable.jsx
@@ -1,4 +1,4 @@
-import { Button, Table } from "@heroui/react";
+import { AlertDialog, Button, Table } from "@heroui/react";
 import Link from "next/link";
 import React from "react";
 
@@ -26,9 +26,37 @@ const UsersTable = ({ users }) => {
                   <Link href={`/users/${user._id}`}>
                     <Button variant="outline">Edit</Button>
                   </Link>
-                  <Link href={`/users/${user._id}`}>
-                    <Button variant="danger">Delete</Button>
-                  </Link>
+                  <AlertDialog>
+                    <Button variant="danger">Delete </Button>
+                    <AlertDialog.Backdrop>
+                      <AlertDialog.Container>
+                        <AlertDialog.Dialog className="sm:max-w-[400px]">
+                          <AlertDialog.CloseTrigger />
+                          <AlertDialog.Header>
+                            <AlertDialog.Icon status="danger" />
+                            <AlertDialog.Heading>
+                              Delete User permanently?
+                            </AlertDialog.Heading>
+                          </AlertDialog.Header>
+                          <AlertDialog.Body>
+                            <p>
+                              This will permanently delete{" "}
+                              <strong>{user.name}</strong> and all of its
+                              data. This action cannot be undone.
+                            </p>
+                          </AlertDialog.Body>
+                          <AlertDialog.Footer>
+                            <Button slot="close" variant="tertiary">
+                              Cancel
+                            </Button>
+                            <Button slot="close" variant="danger">
+                             Confirm Delete
+                            </Button>
+                          </AlertDialog.Footer>
+                        </AlertDialog.Dialog>
+                      </AlertDialog.Container>
+                    </AlertDialog.Backdrop>
+                  </AlertDialog>
                 </Table.Cell>
               </Table.Row>
             ))}
